bf231c77471977c6f5433825db6ee61e2759ebc7
pj_zhang <758041985@qq.com>
2020-03-26 13:54:20 +0800

    提交

---
+++ code/datastructure/src/main/java/com/self/datastructure/tree/huffman/HuffmanTree.java
@@ -0,0 +1,18 @@
+package com.self.datastructure.tree.huffman;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 霍夫曼树
+ *
+ * @author PJ_ZHANG
+ * @create 2020-03-26 10:36
+ **/
+public class HuffmanTree {
+
+    public static void main(String[] args) {
+    }
+
+}
