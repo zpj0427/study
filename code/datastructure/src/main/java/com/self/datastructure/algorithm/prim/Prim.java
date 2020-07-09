3176e764fe19e29ef8d3e443078f83e801b99d8f
pj_zhang <758041985@qq.com>
2020-07-09 18:14:35 +0800

    提交

---
+++ code/datastructure/src/main/java/com/self/datastructure/algorithm/prim/Prim.java
@@ -0,0 +1,26 @@
+package com.self.datastructure.algorithm.prim;
+
+import lombok.Data;
+
+/**
+ * 普里姆算法
+ * @author PJ_ZHANG
+ * @create 2020-07-09 15:01
+ **/
+public class Prim {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static String prim() {
+
+        return null;
+    }
+
+    @Data
+    static class MyGraph {
+
+    }
+
+}
